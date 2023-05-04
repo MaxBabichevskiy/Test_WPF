32bd5f17c0afcd8c0f949904b401c9fe48566530
MaxBabichevskyi <babichevskiym@gmail.com>
2023-05-04 17:47:10 +0300

    make logic

--- Test_WPF/MainWindow.xaml.cs
+++ Test_WPF/MainWindow.xaml.cs
@@ -24,5 +24,43 @@ namespace Test_WPF
         {
             InitializeComponent();
         }
+
+        private void CheckAnswers_Click(object sender, RoutedEventArgs e)
+        {
+            int score = 0;
+
+            if (q1Answer2.IsChecked == true)
+            {
+                score++;
+            }
+
+            if (q2Answer2.IsChecked == true)
+            {
+                score++;
+            }
+
+            if (q3Answer2.IsChecked == true)
+            {
+                score++;
+            }
+
+            if (q4Answer2.IsChecked == true)
+            {
+                score++;
+            }
+
+            if (q5Answer1.IsChecked == true)
+            {
+                score++;
+            }
+
+            if (q6Answer3.IsChecked == true)
+            {
+                score++;
+            }
+
+            resultLabel.Content = $"Вы ответили правильно на {score} из 6 вопросов.";
+
+        }
     }
 }
